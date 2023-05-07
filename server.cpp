b7dd46e9d9e854a5f95c5eccb33d3e33c397b2bb
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-05-07 18:41:52 +0100

    working on some bot functionalities

--- server.cpp
+++ server.cpp
@@ -3,8 +3,58 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#define BUFFER_SIZE 1024
 
-const int PORT = 1337;
+const int PORT = 6968;
+
+void sendMsg(int fd, const std::string &msg)
+{
+    //     char buffer[1024];
+    //     memset(buffer, 0, sizeof(buffer));
+    //     strcpy(buffer, msg.c_str());
+    //     write(fd, buffer, sizeof(buffer));
+    int ret = send(fd, msg.c_str(), msg.length(), 0);
+    if (ret == -1 && (errno == EPIPE || errno == ECONNRESET))
+    {
+        close(fd);
+        fd = -1;
+    }
+}
+
+void printCurrentTime(int fd)
+{
+    std::time_t currentTime = std::time(nullptr);
+    std::string timeString = std::ctime(&currentTime);
+    sendMsg(fd, timeString);
+}
+
+std::vector<std::string> readLinesFromFile(const std::string &filename)
+{
+    std::vector<std::string> lines;
+    std::ifstream file(filename);
+    if (file.is_open())
+    {
+        std::string line;
+        while (std::getline(file, line))
+            lines.push_back(line);
+        file.close();
+    }
+    return lines;
+}
+
+void FactGen(int fd)
+{
+    std::string filename = "obscureunsettlingfacts.txt";
+    std::vector<std::string> lines = readLinesFromFile(filename);
+    std::srand(std::time(0));
+    int randomIndex = std::rand() % lines.size();
+    sendMsg(fd, lines[randomIndex]);
+}
 
 int main()
 {
@@ -50,11 +100,13 @@ int main()
 
         memset(buffer, 0, sizeof(buffer));
         read(client_sfd, buffer, sizeof(buffer));
-        std::cout << "Client: " << buffer << std::endl;
-
-        memset(buffer, 0, sizeof(buffer));
-        std::cin.getline(buffer, sizeof(buffer));
-        write(client_sfd, buffer, sizeof(buffer));
+        if (strcmp(buffer, "fact") == 0)
+        {
+            std::cout << "FactGen" << std::endl;
+            FactGen(server_sfd);
+        }
+        else if (strcmp(buffer, "time") == 0)
+            printCurrentTime(client_sfd);
     }
 
     close(server_sfd);
