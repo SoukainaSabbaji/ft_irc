4cbe1c3f030f1abd16e45908588eae9471e319fd
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-05-10 16:59:38 +0100

    no merging only chaos

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "server.hpp"
+
+bool validatePort(const std::string &port, const std::string &pass)
+{
+    for (std::string::const_iterator it = port.begin(); it != port.end(); ++it)
+    {
+        if (!isdigit(*it))
+            return false;
+    }
+    int portValue = atoi(port.c_str());
+    return (portValue >= 1 && portValue <= 65535);
+}
+
+bool validatePassword(const std::string &password, const std::string &pass)
+{
+    return (password == pass);
+}
+
+int main(int argc , char **argv)
+{
+    if (argc != 3)
+    {
+        std::cout << RED << "Usage: " << argv[0] << " <port> <password>" << RESET << std::endl;
+        return 1;
+    }
+    std::string port = argv[1];
+    std::string pass = argv[2];
+    if (!validatePort(port, pass))
+    {
+        std::cout << RED << "Invalid port" << RESET << std::endl;
+        return 1;
+    }
+    else if (!validatePassword(pass, pass))
+    {
+        std::cout << RED << "Invalid password" << RESET << std::endl;
+        return 1;
+    }
+    else 
+    {
+        Server server(atoi(port.c_str()), pass);
+        server.run();
+    }
+}
